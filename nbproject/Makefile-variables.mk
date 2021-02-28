664ea7e8e44cc2cf1715ebaa8714726da96a467f
BrianChiu180 <71215856+BrianChiu180@users.noreply.github.com>
2021-02-28 14:53:35 +0800

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=Gnueabi-Windows
+CND_ARTIFACT_DIR_Release=dist/V3/App
+CND_ARTIFACT_NAME_Release=mainmenu
+CND_ARTIFACT_PATH_Release=dist/V3/App/mainmenu
+CND_PACKAGE_DIR_Release=dist/Release/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Release=mainmenu.tar
+CND_PACKAGE_PATH_Release=dist/Release/Gnueabi-Windows/package/mainmenu.tar
+# Debug configuration
+CND_PLATFORM_Debug=Gnueabi-Windows
+CND_ARTIFACT_DIR_Debug=dist/V3/Debug
+CND_ARTIFACT_NAME_Debug=mainmenu
+CND_ARTIFACT_PATH_Debug=dist/V3/Debug/mainmenu
+CND_PACKAGE_DIR_Debug=dist/Debug/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Debug=mainmenu.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Gnueabi-Windows/package/mainmenu.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
